9ec19574a5e34ed1d3c6876611d25b1fb1c26a19
Sebastian Saldarriaga <Sebastian@localhost.localdomain>
2018-11-09 10:48:34 -0500

    Adding webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'ubidots-html-canvas.js',
+    library: 'UbidotsHtmlCanvas',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015'],
+        },
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  devtool: 'source-map',
+};
