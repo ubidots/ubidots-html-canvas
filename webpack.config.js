0eb8ddc988af3b4803fa54596a8a407620625c68
Sebastian Saldarriaga <Sebastian@localhost.localdomain>
2018-11-14 18:53:35 -0500

    Install @babel/plugin-proposal-class-properties plugin

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: './index.js',
+  entry: './src/Ubidots.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'ubidots-html-canvas.js',
-    library: 'UbidotsHtmlCanvas',
+    library: 'Ubidots',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
   },
   module: {
     rules: [
@@ -16,6 +18,7 @@ module.exports = {
         loader: 'babel-loader',
         query: {
           presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-proposal-class-properties']
         },
       },
     ],
