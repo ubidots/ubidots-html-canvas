f0033632fc8f377e34aeeb1615b1f4410cc02f71
Cristian Arrieta <cristian@ubidots.com>
2025-05-09 13:12:41 -0500

    [OP-4170] DX: Adding dev server

--- webpack.config.js
+++ webpack.config.js
@@ -26,4 +26,27 @@ module.exports = {
     colors: true,
   },
   devtool: "source-map",
+  watchOptions: {
+    ignored: /node_modules/,
+    aggregateTimeout: 300,
+    poll: 1000,
+  },
+  devServer: {
+    static: [
+      {
+        directory: path.join(__dirname, 'build'),
+      },
+      {
+        directory: path.join(__dirname, 'public'),
+      }
+    ],
+    compress: true,
+    port: 9100,
+    hot: true,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+      "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+    },
+  },
 };
