6ffd55ab0aa75d383b8ca33af7b144dfb0aabe1d
Ricardo Rito Anguiano <52052247+Ricardoaar@users.noreply.github.com>
2026-01-22 07:07:14 -0600

    Front/pb_1405_add_support_to_html_canvas_v2 (#69)
    * :bug: [OP-4765] Events: Add Events v2
    
    * :sparkles: [OP-4765] Events: Add Support for widget RT handling dta and variables
    
    * :sparkles: [OP-4765] Events: Add Support for v2 disptach events
    
    * :sparkles: [OP-4765] Events: Fix eslint
    
    * :sparkles: [OP-4765] Events: Fix prettoier
    
    * :white_check_mark: [PB-1403] Canvas: Fix tests
    
    * :recycle: [PB-1403] Canvas: rename devices events
    
    * :recycle: [PB-1403] Canvas: Add widget  loaded
    
    * :recycle: [PB-1403] Canvas: IMprove ready state definition
    
    * :recycle: [PB-1403] Canvas: IMprove ready state definition
    
    * :recycle: [PB-1403] Canvas: Improve widget specific events
    
    ---------
    
    Co-authored-by: Ricardo Rito <ricardo.rito@ubidots.com>

--- src/Widget.js
+++ src/Widget.js
@@ -1,6 +1,9 @@
 export class Widget {
   #settings = {};
   #id = '';
+  #variables = [];
+  #data = null;
+  #error = null;
 
   constructor(id) {
     this.#settings = window._pluginWidgetSettings || {};
@@ -13,5 +16,53 @@ export class Widget {
 
   getId() {
     return this.#id;
+  }
+
+  /**
+   * Get the widget variables
+   * @returns {Array} The widget variables
+   */
+  getVariables() {
+    return this.#variables;
+  }
+
+  /**
+   * Set the widget variables
+   * @param {Array} variables - The variables to set
+   */
+  setVariables(variables) {
+    this.#variables = variables;
+  }
+
+  /**
+   * Get the widget data
+   * @returns {*} The widget data
+   */
+  getData() {
+    return this.#data;
+  }
+
+  /**
+   * Set the widget data
+   * @param {*} data - The data to set
+   */
+  setData(data) {
+    this.#data = data;
+  }
+
+  /**
+   * Get the widget error
+   * @returns {*} The widget error
+   */
+  getError() {
+    return this.#error;
+  }
+
+  /**
+   * Set the widget error
+   * @param {*} error - The error to set
+   */
+  setError(error) {
+    this.#error = error;
   }
 }
